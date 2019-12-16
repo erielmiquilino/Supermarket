71695b2c23c2b0cecad66b4dc1f1a40b56f4b6af
Eriel Miquilino <erielmiquilino@hotmail.com>
2019-12-16 03:09:25 -0300

    Ajuste na baseEntity
    Inicio da implementação da camada de Infra

---
+++ Supermarket.Data/Context/MyContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Supermarket.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket.Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<CartItem> CartItems { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> options) : base (options)
+        {
+
+        }
+    }
+}
--- Supermarket.Domain/Entities/Base/BaseEntity.cs
+++ Supermarket.Domain/Entities/Base/BaseEntity.cs
@@ -4,8 +4,14 @@ namespace Supermarket.Domain.Entities.Base
 {
     public class BaseEntity
     {
-        public virtual Guid Id { get; set; }
+        public Guid Id { get; set; }
 
-        public virtual DateTime InsertDate { get; set; }
-    }
+		private DateTime? _insertDate;
+
+		public DateTime? InsertDate
+		{
+			get { return _insertDate; }
+			set { _insertDate = (value == null ? DateTime.UtcNow : value); }
+		}
+	}
 }
