425c58eb16106fe98458d97bb6c00dc33a57cfe9
Eriel Miquilino <erielmiquilino@hotmail.com>
2019-12-28 20:38:53 -0300

    Implementada classe de repositorio generica

---
+++ Supermarket.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using Supermarket.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Domain.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> InsertAsysnc(T Item);
+
+        Task<T> UpdateAsync(T item);
+
+        Task<bool> DeleteAsync(Guid id);
+
+        Task<T> SelectAsync(Guid id);
+
+        Task<IEnumerable<T>> SelectAsync();
+    }
+}
