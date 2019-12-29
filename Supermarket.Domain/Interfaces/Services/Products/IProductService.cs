1ea99c390ed56e7c03e0a0e492a6275d4772f363
Eriel Miquilino <erielmiquilino@hotmail.com>
2019-12-28 21:49:59 -0300

    Implementado interface de serviço para Product Implementada classe de metodos de serviço para product

---
+++ Supermarket.Domain/Interfaces/Services/Products/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using Supermarket.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Domain.Interfaces.Services.Products
+{
+    public interface IProductService
+    {
+        Task<Product> Get(Guid id);
+
+        Task<IEnumerable<Product>> GetAll();
+
+        Task<Product> Post(Product product);
+
+        Task<Product> Put(Product product);
+
+        Task<bool> Delete(Guid id);
+    }
+}
---
+++ Supermarket.Servic/Services/Products/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using Supermarket.Domain.Interfaces;
+using Supermarket.Domain.Interfaces.Services.Products;
+using Supermarket.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Service.Services.Products
+{
+    public class ProductService : IProductService
+    {
+        private IRepository<Product> _repository;
+
+        public ProductService(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<Product> Get(Guid id)
+        {
+            return await _repository.SelectAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return await _repository.SelectAsync();
+        }
+
+        public async Task<Product> Post(Product product)
+        {
+            return await _repository.InsertAsysnc(product);
+        }
+
+        public async Task<Product> Put(Product product)
+        {
+            return await _repository.UpdateAsync(product);
+        }
+    }
+}
